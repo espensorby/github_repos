2d7012ce08cd5691d8bda9e76f472eb0710f6346
Espen S <espen@sorby.net>
2024-01-12 12:28:13 +0100

    Fetch repos and display repo names

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+interface RepoProps {
+  id: number;
+  name: string;
+}
 
 const App = () => {
-  
+  const [repos, setRepos] = useState<RepoProps[]>([]);
+
+  const fetchRepos = useCallback(async () => {
+    try {
+      const response: AxiosResponse = await axios.get('https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100');
+      setRepos(response.data.items);
+    } catch (error) {
+      throw new Error(`Failed to fetch repositories: ${error}`);
+    }
+  }, []);
+
+  useEffect(() => { 
+    fetchRepos(); 
+  }, [fetchRepos]);
+
   return (
-    <div>
-      Github repos
-    </div>
+    <>
+      {repos.length !== 0 ? repos.map((repo: RepoProps) => (
+        <div key={repo.id}>
+          {repo.name}
+        </div>
+      )) : <p>Fetching repos...</p>}
+    </>
   )
 }
 
